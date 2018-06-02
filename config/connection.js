b17b1da737b96acfc6dfb151be024f84eda35153
Collier <cole.hansen@gmail.com>
2018-06-02 09:57:29 -0700

    Add JawsDB info

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
+  user: "uloqvtuz43yzq9uf",
+  password: "gsnz0hc69vdunwbs",
+  database: "gfdo64lp1kxdilii"
 });
 
 // Make connection.
